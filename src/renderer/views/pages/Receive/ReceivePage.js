ca569d731d7981d82faad03f217b2490f6289741
nijynot <nijynot@gmail.com>
2019-02-23 19:44:16 +0100

    init ReceivePage

---
+++ src/renderer/views/pages/Receive/ReceivePage.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+require('./ReceivePage.scss');
+
+export default function ReceivePage(props) {
+  return (
+    <div className="ReceivePage">
+      Init
+    </div>
+  )
+}
